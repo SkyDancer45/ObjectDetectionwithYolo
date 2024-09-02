7c90bbbf34bff4b52c5cb2678067b6ab212252e3
eag <kishupiarwga>
2024-09-02 23:00:19 +0530

    thermo

---
+++ predict.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+model = YOLO("best.pt")
+
+# Run batched inference on a list of images
+results = model(["Photo-4.jpeg"])  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    result.show()  # display to screen
